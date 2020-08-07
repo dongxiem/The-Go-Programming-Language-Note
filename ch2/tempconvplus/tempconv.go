f0c7841fe3d7cc4d3851006d211dcf3862fcebdc
dongxiem <ljmiaw@163.com>
2020-08-08 00:15:48 +0800

    Add tempconvplus

---
+++ ch2/tempconvplus/tempconv.go
@@ -0,0 +1,16 @@
+package tempconvplus
+
+import "fmt"
+
+// 将变量的声明、对应的常量，还有方法都放到当前tempconv.go的源文件中
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
