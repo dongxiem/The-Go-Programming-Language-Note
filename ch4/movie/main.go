5034fcb07f8da04c36edf1c6ce5bb0228fa8d137
dongxiem <ljmiaw@163.com>
2020-08-11 20:25:43 +0800

    Add movie

---
+++ ch4/movie/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// Movie 数据结构
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+// 新建数据
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	// ...
+}
+
+func main() {
+	{
+		//!+Marshal
+		// 将一个类似movies的结构体slice转为JSON的过程叫编组（marshaling）
+		data, err := json.Marshal(movies)
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		fmt.Printf("%s\n", data)
+		//!-Marshal
+	}
+
+	{
+		//!+MarshalIndent
+		// MarshalIndent函数将产生整齐缩进的输出
+		data, err := json.MarshalIndent(movies, " ", "    ")
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		fmt.Printf("%s\n", data)
+		//!-MarshalIndent
+
+		//!+Unmarshal
+		var titles []struct{ Title string }
+		if err := json.Unmarshal(data, &titles); err != nil {
+			log.Fatalf("JSON unmarshaling failed: %s", err)
+		}
+		fmt.Println(titles) // "[{Casablanca} {Cool Hand Luke} {Bullitt}]"
+		//!-Unmarshal
+	}
+}
